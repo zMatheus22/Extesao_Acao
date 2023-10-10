3c206ae33735825feb15ad6eaa81bb55da07e150
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2023-10-10 15:45:00 -0300

    Add o cálculos de Dividendo Projetado e Preço Teto

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,13 @@ function calculoGraham(lpa, vpa) {
   // √ (22,5 x LPA x VPA)
   return Math.sqrt(22.5 * lpa * vpa).toFixed(2);
 }
+
+function calculoDividendoProjetado(lpa, payout) {
+  // LPA x PAYOUT
+  return (lpa * (payout / 100)).toFixed(2);
+}
+
+function calculoPrecoteto(calculoDPA, dividendoDesejado) {
+  // Dividendo Projetado ÷ DY Desejato
+  return calculoDPA / (dividendoDesejado / 100);
+}
